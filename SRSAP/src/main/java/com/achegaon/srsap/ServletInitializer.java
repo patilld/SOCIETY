541fafef2c2ea93ce75b5e7725255ffd395aaba1
patilld <patilld94@gmail.com>
2018-07-22 15:39:36 +0530

    Added Shreeram society project

---
+++ SRSAP/src/main/java/com/achegaon/srsap/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.achegaon.srsap;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SrsapApplication.class);
+	}
+
+}
---
+++ SRSAP/src/test/java/com/achegaon/srsap/SrsapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.achegaon.srsap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SrsapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
